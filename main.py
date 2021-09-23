02e6a45467fea07cf0854dfb915e473ff11e8783
Michal1898 <michal.volf@gmail.com>
2021-09-23 15:32:01 +0200

    more pythonic solution.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def make_fancy(text, n):
-    pointer = 0
-    for i in text:
-        print(pointer * " ", n * i)
-        pointer += 1
-
+def make_fancy(text:str, n):
+    letters_list = list(text)
+    space_on_the_beginning=""
+    while len(letters_list):
+        one_letter=letters_list.pop(0)
+        print(space_on_the_beginning,n*one_letter)
+        space_on_the_beginning += " "
 
 if __name__ == "__main__":
-    make_fancy("KOKOT", 7)
+    make_fancy("LOKOMOTIVA", 2)
