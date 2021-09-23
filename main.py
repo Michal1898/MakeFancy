f7c3cb92c0cb109d1d83a12f6161ebc86ee1a42a
Michal1898 <michal.volf@gmail.com>
2021-09-23 16:31:30 +0200

    Another pythonic solution.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def make_fancy(text:str, n):
-    letters_list = list(text)
-    space_on_the_beginning=""
-    while len(letters_list):
-        one_letter=letters_list.pop(0)
-        print(space_on_the_beginning,n*one_letter)
+def make_fancy(text: str, n):
+    space_on_the_beginning = ""
+    while len(text):
+        print(space_on_the_beginning, n * text[0])
         space_on_the_beginning += " "
+        text = text[1:]
+
 
 if __name__ == "__main__":
     make_fancy("LOKOMOTIVA", 2)
